a10b53a0e470095ecf14353bdcd28a8350987e6b
Zachary Lai <ilzy@foxmail.com>
2023-03-02 08:32:24 +0000

    openai

--- WrapperFunction/__init__.py
+++ WrapperFunction/__init__.py
@@ -1,14 +1,9 @@
 import logging
 import azure.functions as func
-from FastAPIApp import app  # Main API application
 import requests
-# ###############
-url = "https://rapid-translate-multi-traduction.p.rapidapi.com/t"
-headers = {
-	"content-type": "application/json",
-	"X-RapidAPI-Key": "0dd8668bb4msh7d84ecd476335fep118479jsn6d1e00ff27b0",
-	"X-RapidAPI-Host": "rapid-translate-multi-traduction.p.rapidapi.com"
-}
+import openai
+from FastAPIApp import app  # Main API application
+
 # ############### 
 
 @app.get("/sample")
@@ -24,17 +19,40 @@ async def get_name(name: str):
         "name": name,
     }
 
-@app.get("/chat/{chat}")
-async def get_chat(chat: str):
+# Rapid API
+url = "https://rapid-translate-multi-traduction.p.rapidapi.com/t"
+headers = {
+	"content-type": "application/json",
+	"X-RapidAPI-Key": "0dd8668bb4msh7d84ecd476335fep118479jsn6d1e00ff27b0",
+	"X-RapidAPI-Host": "rapid-translate-multi-traduction.p.rapidapi.com"
+}
+@app.get("/translate/{translate}")
+async def get_translate(translate: str):
     payload = {
 	"from": "en",
 	"to": "zh",
 	"e": "",
-	"q": chat
+	"q": translate
     }
     response = requests.request("POST", url, json=payload, headers=headers, timeout = 30)
     return {
-        "chat": response.text,
+        "translate": response.text,
+    }
+
+# OpenAI
+
+openai.api_key = "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"
+
+@app.get("/chat/{chat}")
+async def get_chat(chat: str):
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": chat}
+        ]
+        )
+    return {
+        "chat": completion.choices[0].message,
     }
 
 async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
